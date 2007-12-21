f1f9b10124f22acf1ab88759aa861ed41e62cb45
serpent.speak <serpent.speak@b387b25f-291c-0410-8dc7-e51caacd5b47>
2007-12-21 18:54:54 +0000

    python as p-language

--- contrib/serpent.speak/trunk/Q/setup.py
+++ contrib/serpent.speak/trunk/Q/setup.py
@@ -20,11 +20,13 @@ class qinstall_lib(install_lib):
         dist = self.distribution
         qdst = os.path.join(dist.qhome, dist.qarch)
         # TODO: eliminate hard-coded names
-        pyfiles = ['_k.so', 'q.py', 'qc.py']
+        pyfiles = ['_k.so', 'q.py', 'qc.py',]
         if os.path.isdir(self.build_dir):
             outfiles = [self.copy_file(os.path.join(self.build_dir, f),
                                        self.install_dir)[0] for f in pyfiles]
             outfiles += [self.copy_file(os.path.join(self.build_dir, 'py.so'), qdst)[0]]
+            outfiles += [self.copy_file(os.path.join(self.build_dir, 'p.so'), qdst)[0]]
+            outfiles += [self.copy_file('p.k', dist.qhome)[0]]
         else:
             self.warn("'%s' does not exist" % self.build_dir)
             return
@@ -38,6 +40,12 @@ module_k = Extension('_k',
                      include_dirs = [ ],)
 modulepy = QExtension('py',
                       sources=[ 'py.c',],
+                      extra_compile_args = ['-g'],
+                      runtime_library_dirs = [python_lib_dir],
+                      )
+
+modulepy = QExtension('p',
+                      sources=[ 'p.c',],
                       extra_compile_args = ['-g'],
                       runtime_library_dirs = [python_lib_dir],
                       )
