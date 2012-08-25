4c25a0821dff73e3ca650519fc9e132a110070f3
serpent.speak <serpent.speak@b387b25f-291c-0410-8dc7-e51caacd5b47>
2012-08-25 19:21:07 +0000

    Py3K: added abicode libpython, decode argv, buffer

--- contrib/serpent.speak/trunk/Q/py.c
+++ contrib/serpent.speak/trunk/Q/py.c
@@ -3,10 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dlfcn.h>
+#ifdef PY3K
+static int (*Py_Main)(int argc, wchar_t **argv);
+static wchar_t *(*c2w)(char*, size_t *);
+static void (*PyMem_Free)(void *);
+#else
 static int (*Py_Main)(int argc, char **argv);
+#endif
 K py(K f, K x, K lib)
 {
-	char *error;
+	int argc; char **argv;
+	char *error, *p, *buf;
 	void *h;
 	K r;
 	h = dlopen((const char *)kG(lib), RTLD_NOW|RTLD_GLOBAL);
@@ -21,17 +28,32 @@ K py(K f, K x, K lib)
 	   K y;
 	   P((y = kK(x)[i])->t!=KC, krr("arg type"));
 	   m += y->n+1);
-	char** const argv = malloc(sizeof(char*) * (xn+1));
+	argc = xn+1;
+	argv = malloc(sizeof(char*) * argc);
 	P(!argv, krr("memory"));
-        char* const buf = malloc(m);
+        buf = malloc(m);
 	P(!buf,(free(argv),krr("memory")));
 	argv[0] = f->s;
-	char* p = buf;
+	p = buf;
 	DO(xn,
 	   K y = kK(x)[i]; 
 	   argv[i+1] = memcpy(p, kG(y), y->n);
 	   p += y->n; *p++ = '\0');
-	r = ki(Py_Main(xn+1, argv));
+#ifdef PY3K
+	dlerror();    /* Clear any existing error */
+        c2w = dlsym(h, "_Py_char2wchar");
+        P((error = dlerror()),krr(error));
+	dlerror();    /* Clear any existing error */
+        PyMem_Free = dlsym(h, "PyMem_Free");
+        P((error = dlerror()),krr(error));
+	wchar_t **wargv = malloc(sizeof(wchar_t*)*argc);
+	DO(argc,P(!(wargv[i]=c2w(argv[i],NULL)),krr("decode")));
+	r = ki(Py_Main(argc, wargv));
+	DO(argc,PyMem_Free(wargv[i]));
+	free(wargv);
+#else
+	r = ki(Py_Main(argc, argv));
+#endif
 	dlclose(h);
 	free(argv);
 	free(buf);
--- contrib/serpent.speak/trunk/Q/python.q
+++ contrib/serpent.speak/trunk/Q/python.q
@@ -1,8 +1,9 @@
 #! /usr/bin/env q
 PYVER:"2.7"
+PYSO:`py
 
 args:{$["@"~last x;-1_x;x]} each .z.x
-py:`py 2:(`py;3)
+py:PYSO 2:(`py;3)
 lib:"libpython",PYVER,".so\000"
 `QVER setenv .Q.fmt[3;1].Q.k
 r:py[.z.f;args;lib]
