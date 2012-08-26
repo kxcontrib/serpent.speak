d38c00834285fc12433bd985de59df4b36fbb1f0
serpent.speak <serpent.speak@b387b25f-291c-0410-8dc7-e51caacd5b47>
2012-08-26 16:17:25 +0000

    Don't use deprecated API

--- contrib/serpent.speak/trunk/Q/tests/testq.py
+++ contrib/serpent.speak/trunk/Q/tests/testq.py
@@ -4,23 +4,23 @@ q("\\e 0") # disable q's debug on error
 KXVER=int(q('.Q.k'))
 class CallTestCase(unittest.TestCase):
     def test_a0(self):
-        self.failUnlessEqual(q('{1}')(), q('1'))
+        self.assertEqual(q('{1}')(), q('1'))
     def test_a1(self):
-        self.failUnlessEqual(q('::')(1), q('1i'))
+        self.assertEqual(q('::')(1), q('1i'))
     def test_a2(self):
-        self.failUnlessEqual(q('+')(1,2), q('3i'))
+        self.assertEqual(q('+')(1,2), q('3i'))
     def test_a3(self):
-        self.failUnlessEqual(q('?')(q('10b'), 1, 2), q('1 2i'))
+        self.assertEqual(q('?')(q('10b'), 1, 2), q('1 2i'))
     def test_err(self):
         try:
             q("{'`test}", 0)
         except kerr, e:
-            self.failUnlessEqual(str(e), 'test')
+            self.assertEqual(str(e), 'test')
         q("f:{'`test}")
         try:
             q("{f[x]}")(42)
         except kerr, e:
-            self.failUnlessEqual(str(e), 'test')
+            self.assertEqual(str(e), 'test')
 
 if KXVER >= 3:
     class TestGUID(unittest.TestCase):
