92b367f79547cd406d3be97a34a4613d256def22
serpent.speak <serpent.speak@b387b25f-291c-0410-8dc7-e51caacd5b47>
2012-08-26 02:41:21 +0000

    all tests pass under python 2.7 and 3.2

--- Q/py.c
+++ Q/py.c
@@ -47,9 +47,12 @@ K py(K f, K x, K lib)
         PyMem_Free = dlsym(h, "PyMem_Free");
         P((error = dlerror()),krr(error));
 	wchar_t **wargv = malloc(sizeof(wchar_t*)*argc);
+	wchar_t **wargv_copy = malloc(sizeof(wchar_t*)*argc);
 	DO(argc,P(!(wargv[i]=c2w(argv[i],NULL)),krr("decode")));
+	memcpy(wargv_copy, wargv, sizeof(wchar_t*)*argc); 
 	r = ki(Py_Main(argc, wargv));
-	DO(argc,PyMem_Free(wargv[i]));
+	DO(argc,PyMem_Free(wargv_copy[i]));
+	free(wargv_copy);
 	free(wargv);
 #else
 	r = ki(Py_Main(argc, argv));
